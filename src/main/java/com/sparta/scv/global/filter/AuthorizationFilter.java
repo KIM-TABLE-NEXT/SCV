8b241382d82a3650b439d1d5d31fdea2c1edfb4f
sehun <harrisbang2@gmail.com>
2024-03-18 20:46:17 +0900

    feat : 유저 정보 변경 / 유저 삭제

--- src/main/java/com/sparta/scv/global/filter/AuthorizationFilter.java
+++ src/main/java/com/sparta/scv/global/filter/AuthorizationFilter.java
@@ -35,19 +35,21 @@ public class AuthorizationFilter extends
     //토큰 받기
     String token = jwtUtil.getTokenFromRequest(httpServletRequest);
 
-    if (Objects.nonNull(token)) { // 만약 토큰이 있으면
+    if (Objects.nonNull(token)) {
+      // 만약 토큰이 있으면
       if (jwtUtil.validateToken(token)) {
-        // 토큰에서 유저정보를 받아와서 info에 넣음
-        Claims info = jwtUtil.getUserInfoFromToken(token);
+        // 토큰에서 유저정보를 받아와서 info 에 넣음
+        String t = token.substring(6);
+        Claims info = jwtUtil.getUserInfoFromToken(t);
 
-        // info 에서 이메일 을 뽑아와서 저장
-        String email = info.getSubject();
+        // info 에서 아이디 을 뽑아와서 저장
+        String username = info.getSubject();
 
         SecurityContext context = SecurityContextHolder.createEmptyContext();
         // Authentication (인증된 정보) 를 담고 있는 Holder
         // SecurityContextHolder 가 가진 값을 통해 인증이 되었나 확인할 수있음.
 
-        UserDetails userDetails = userDetailsServiceImpl.getUserDetails(email);
+        UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(username);
         //userDetails 에 유저의 상세 정보를 넣음
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails,
@@ -65,17 +67,11 @@ public class AuthorizationFilter extends
         httpServletResponse.setContentType("application/json; charset=UTF-8");
         httpServletResponse.getWriter()
             .write("토큰이 유효하지 않습니다.");
-        // write 타입은 스트링 이므로 ,objectMapper 를 통해 맵핑 해서 String 으로변환
         return;
         // 더 필터를 타지 않고 에러응답을 리턴시킴
       }
     }
-
     filterChain.doFilter(httpServletRequest, httpServletResponse);
-
-
   }
-
-
 }
 
--- src/main/java/com/sparta/scv/user/controller/UserController.java
+++ src/main/java/com/sparta/scv/user/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.sparta.scv.user.controller;
 
+import com.sparta.scv.global.impl.UserDetailsImpl;
 import com.sparta.scv.user.dto.LoginRequestDto;
 import com.sparta.scv.user.dto.SignupDto;
+import com.sparta.scv.user.dto.UpdateRequestDto;
 import com.sparta.scv.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
@@ -17,17 +22,31 @@ public class UserController {
   private final UserService userService;
 
   @PostMapping("/users/signup")
-  public ResponseEntity<SignupDto> signup(@RequestBody SignupDto requestDto) {
+  public ResponseEntity<SignupDto> SignUp(@RequestBody SignupDto requestDto) {
     return  ResponseEntity
         .status(HttpStatus.CREATED)
         .body(userService.signup(requestDto));
   }
   @PostMapping("/users/login")
-  public ResponseEntity<Long> signup(@RequestBody LoginRequestDto requestDto,
+  public ResponseEntity<Long> Login(@RequestBody LoginRequestDto requestDto,
       HttpServletResponse res) {
     return  ResponseEntity
         .status(HttpStatus.OK)
         .body(userService.login(requestDto,res));
+  }
+  @PutMapping("/users/updateprofile")
+  public ResponseEntity<Long> updateProfile(@RequestBody UpdateRequestDto requestDto,
+      @AuthenticationPrincipal UserDetailsImpl impl){
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(userService.update(requestDto,impl.getUser()));
+  }
+  @DeleteMapping("/users/delete")
+  public ResponseEntity<Long> deleteAccount(
+      @AuthenticationPrincipal UserDetailsImpl impl){
+    return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(userService.delete(impl.getUser()));
   }
 
 }
