609504d9b7b3f242f35e1ec0d47d0b12686b3d8d
SerenityZenDev <tjrqo2000@gmail.com>
2024-03-19 16:04:08 +0900

    :sparkles: (Board, BoardMember) Controller swagger Annotation 추가

--- src/main/java/com/sparta/scv/boardmember/BoardMemberController.java
+++ src/main/java/com/sparta/scv/boardmember/BoardMemberController.java
@@ -1,6 +1,8 @@
 package com.sparta.scv.boardmember;
 
 import com.sparta.scv.global.impl.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -13,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/boardmembers")
 @RequiredArgsConstructor
+@Tag(name = "보드 회원 관리", description = "보드 회원 관리에 관련된 API")
 public class BoardMemberController {
 
     private final BoardMemberService boardMemberService;
 
     @PostMapping
+    @Operation(summary = "보드 멤버 추가", description = "보드에서 활동할 유저를 추가합니다.")
     public ResponseEntity<Long> addBoardMember(
         @RequestBody BoardMemberRequest boardMemberRequest,
         @AuthenticationPrincipal UserDetailsImpl userDetails
@@ -28,6 +32,7 @@ public class BoardMemberController {
     }
 
     @DeleteMapping
+    @Operation(summary = "보드 멤버 삭제", description = "보드에서 활동 중인 유저를 삭제합니다.")
     public ResponseEntity<Long> deleteBoardMember(
         @RequestBody BoardMemberRequest boardMemberRequest,
         @AuthenticationPrincipal UserDetailsImpl userDetails
