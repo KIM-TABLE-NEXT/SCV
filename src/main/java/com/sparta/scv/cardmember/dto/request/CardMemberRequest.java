ffb8831eee08862f7138c709074f03470bdeee37
kim_tablenext <bybycu118@gmail.com>
2024-03-20 18:48:48 +0900

    :wrench: Fix(CardMember) : Boardmember 검증 로직 추가

--- src/main/java/com/sparta/scv/cardmember/dto/request/CardMemberRequest.java
+++ src/main/java/com/sparta/scv/cardmember/dto/request/CardMemberRequest.java
@@ -1,11 +1,18 @@
 package com.sparta.scv.cardmember.dto.request;
 
+import com.sparta.scv.board.BoardIdHolder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class CardMemberRequest {
+public class CardMemberRequest implements BoardIdHolder {
+    private Long boardId;
     private Long cardId;
     private Long memberId;
+
+    @Override
+    public Long getBoardId() {
+        return boardId;
+    }
 }
