83944e35d8bc4bf1e5ebd17d111051bdeceafb00
김상엽 <54929479+KIM-TABLE-NEXT@users.noreply.github.com>
2024-03-21 11:14:25 +0900

    Feature/card (#11)
    * :sparkles: Feat(Card) : 카드 CRUD 구현
    
    * :sparkles: Feat(Cardmember) : 카드 멤버 CRUD 구현
    
    * :wrench: Fix(Card) : 카드 controller response 수정
    
    * :wrench: FIX(Card) : Card 도메인 유저 관련 로직 수정
    
    * :wrench: FIX(Cardmemeber) : Cardmember 도메인 유저 관련 로직 수정
    
    * :wrench: FIX(Card ,Cardmemeber) : 유저 ID로 비교하도록 로직 수정
    
    * :sparkles: Feat(Comment) : Comment 도메인 기능 구현
    
    * :wrench: Fix(Repository) : Repository 함수명 수정
    
    * :wrench: Fix(Import) : Import문 경로 수정
    
    * :wrench: Fix(Comment) : @Transactional 추가, 함수 배치 변경
    
    * :wrench: Fix(Response Dto) : @Getter @Setter 추가
    
    * :wrench: Fix(Card) : 카드 컬럼 변경 함수 분리, BoardMember 검증 추가, CardService 검색함수 private으로 변경
    
    * :wrench: Fix(CardMember) : Boardmember 검증 로직 추가
    
    * :wrench: Fix(Comment) : Boardmember 검증 로직 추가
    
    * :wrench: Fix(WebSecurityConfig) : Swagger url 권한 허가

--- src/main/java/com/sparta/scv/card/entity/Card.java
+++ src/main/java/com/sparta/scv/card/entity/Card.java
@@ -2,6 +2,7 @@ package com.sparta.scv.card.entity;
 
 import com.sparta.scv.boardcolumn.BoardColumn;
 import com.sparta.scv.card.dto.request.CardRequest;
+import com.sparta.scv.card.dto.request.CardUpdateRequest;
 import com.sparta.scv.user.entity.User;
 import jakarta.persistence.*;
 import com.sparta.scv.user.entity.User;
@@ -38,6 +39,12 @@ public class Card {
   @Column(nullable = false)
   private String color;
 
+  @Column(nullable = false)
+  private String startDate;
+
+  @Column(nullable = false)
+  private String endDate;
+
   @ManyToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "column_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
   private BoardColumn column;
@@ -46,18 +53,25 @@ public class Card {
   @JoinColumn(name="owner_id",nullable = false,foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
   private User owner;
 
-  public Card(String title, String description, String color, BoardColumn boardColumn, User user){
-    this.title = title;
-    this.description = description;
-    this.color = color;
+  public Card(CardRequest cardRequest, BoardColumn boardColumn, User user){
+    this.title = cardRequest.getTitle();
+    this.description = cardRequest.getDescription();
+    this.color = cardRequest.getColor();
+    this.startDate = cardRequest.getStartDate();
+    this.endDate = cardRequest.getEndDate();
     this.column = boardColumn;
     this.owner = user;
   }
 
-  public void update(CardRequest cardRequest, BoardColumn boardColumn) {
-    this.title = cardRequest.getTitle();
-    this.description = cardRequest.getDescription();
-    this.color = cardRequest.getColor();
+  public void update(CardUpdateRequest cardUpdateRequest) {
+    this.title = cardUpdateRequest.getTitle();
+    this.description = cardUpdateRequest.getDescription();
+    this.color = cardUpdateRequest.getColor();
+    this.startDate = cardUpdateRequest.getStartDate();
+    this.endDate = cardUpdateRequest.getEndDate();
+  }
+
+  public void move(BoardColumn boardColumn) {
     this.column = boardColumn;
   }
 }
--- src/main/java/com/sparta/scv/comment/dto/request/CommentIdRequest.java
+++ src/main/java/com/sparta/scv/comment/dto/request/CommentIdRequest.java
@@ -1,8 +1,15 @@
 package com.sparta.scv.comment.dto.request;
 
+import com.sparta.scv.board.BoardIdHolder;
 import lombok.Getter;
 
 @Getter
-public class CommentIdRequest {
+public class CommentIdRequest implements BoardIdHolder {
+    private Long boardId;
     private Long cardId;
+
+    @Override
+    public Long getBoardId() {
+        return boardId;
+    }
 }
