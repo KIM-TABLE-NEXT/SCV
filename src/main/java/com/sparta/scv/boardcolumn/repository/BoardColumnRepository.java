5052a175e338eb54b0e1a3f3241851224fadd13e
김상엽 <54929479+KIM-TABLE-NEXT@users.noreply.github.com>
2024-03-22 20:01:28 +0900

    Update BoardColumnRepository.java

--- src/main/java/com/sparta/scv/boardcolumn/repository/BoardColumnRepository.java
+++ src/main/java/com/sparta/scv/boardcolumn/repository/BoardColumnRepository.java
@@ -7,13 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface BoardColumnRepository extends JpaRepository<BoardColumn, Long> {
-
-    BoardColumn findByPosition(Long originPosition);
-
     List<BoardColumn> findByBoardIdOrderByPositionAsc(Long boardId);
 
     @Query("SELECT MAX(c.position) FROM BoardColumn c WHERE c.board.id = :boardId")
     Optional<Long> findMaxPosition(Long boardId);
-
-    List<BoardColumn> findByBoardIdAndPositionGreaterThan(Long boardId, Long position);
 }
